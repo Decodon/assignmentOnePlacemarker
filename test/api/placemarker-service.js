56860f8e27297c6aba5d41f3e27568a4fc7b9fcb
Decodon <20095352@mail.wit.ie>
2022-03-29 21:52:18 +0100

    Exposing the User model via a simple API

--- test/api/placemarker-service.js
+++ test/api/placemarker-service.js
@@ -1,1 +1,27 @@
-export const placemarkerService = {};
+import axios from "axios";
+
+import { serviceUrl } from "../fixtures.js";
+
+export const placemarkerService = {
+  placemarkerUrl: serviceUrl,
+
+  async createUser(user) {
+    const res = await axios.post(`${this.placemarkerUrl}/api/users`, user);
+    return res.data;
+  },
+
+  async getUser(id) {
+    const res = await axios.get(`${this.placemarkerUrl}/api/users/${id}`);
+    return res.data;
+  },
+
+  async getAllUsers() {
+    const res = await axios.get(`${this.placemarkerUrl}/api/users`);
+    return res.data;
+  },
+
+  async deleteAllUsers() {
+    const res = await axios.delete(`${this.placemarkerUrl}/api/users`);
+    return res.data;
+  },
+};
--- test/api/user-api-test.js
+++ test/api/user-api-test.js
@@ -1,8 +1,55 @@
+import { assert } from "chai";
 import { placemarkerService } from "./placemarker-service.js";
+import { assertSubset } from "../test-utils.js";
+import { maggie, testUsers } from "../fixtures.js";
 
 suite("User API tests", () => {
-  setup(async () => {});
+  setup(async () => {
+    await placemarkerService.deleteAllUsers();
+    for (let i = 0; i < testUsers.length; i += 1) {
+      // eslint-disable-next-line no-await-in-loop
+      testUsers[0] = await placemarkerService.createUser(testUsers[i]);
+    }
+  });
   teardown(async () => {});
 
-  test("create a user", async () => {});
+  test("create a user", async () => {
+    const newUser = await placemarkerService.createUser(maggie);
+    assertSubset(maggie, newUser);
+    assert.isDefined(newUser._id);
+  });
+
+  test("delete all users", async () => {
+    let returnedUsers = await placemarkerService.getAllUsers();
+    assert.equal(returnedUsers.length, 3);
+    await placemarkerService.deleteAllUsers();
+    returnedUsers = await placemarkerService.getAllUsers();
+    assert.equal(returnedUsers.length, 0);
+  });
+
+  test("get a user - success", async () => {
+    const returnedUser = await placemarkerService.getUser(testUsers[0]._id);
+    assert.deepEqual(testUsers[0], returnedUser);
+  });
+
+  test("get a user - bad id", async () => {
+    try {
+      const returnedUser = await placemarkerService.getUser("1234");
+      assert.fail("Should not return a response");
+    } catch (error) {
+      assert(error.response.data.message === "No User with this id");
+      assert.equal(error.response.data.statusCode, 503);
+    }
+  });
+
+  test("get a user - deleted user", async () => {
+    await placemarkerService.deleteAllUsers();
+    try {
+      const returnedUser = await placemarkerService.getUser(testUsers[0]._id);
+      assert.fail("Should not return a response");
+    } catch (error) {
+      assert(error.response.data.message === "No User with this id");
+      assert.equal(error.response.data.statusCode, 404);
+    }
+  });
 });
