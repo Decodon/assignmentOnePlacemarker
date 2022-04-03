e3785f373d8bb65c5902c7e396369954b5f15c38
Decodon <20095352@mail.wit.ie>
2022-04-03 19:12:10 +0100

    Error found in db address

--- src/server.js
+++ src/server.js
@@ -40,7 +40,8 @@ if (result.error) {
 
 async function init() {
   const server = Hapi.server({
-    port: process.env.PORT || 3000,
+    port: 3000,
+    host: "localhost",
   });
 
   await server.register(Inert);
